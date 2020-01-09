2257b49a26f6a400edcaead1a36c95b65d613fe5
balajigithub003 <balajikrishnan003@gmail.com>
2020-01-09 08:32:06 +0530

    create class

---
+++ src/org/cts/test/login/SampleB.java
@@ -0,0 +1,11 @@
+package org.cts.test.login;
+
+public class SampleB {
+    public void bankName () {
+    	System.out.println("Bank name is the Canara Bank");
+    }
+    public static void main(String[] args) {
+		SampleB bank = new SampleB();
+		bank.bankName();
+	}
+}
